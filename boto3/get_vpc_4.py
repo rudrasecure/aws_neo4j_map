43d2d341b68ac2a842502b7e35e19eca4dad46ff
Hazzardousmonk <ks@rudrasecure.com>
2023-07-03 21:07:32 +0530

    Add RDS and VPC AWS API scraping scripts

---
+++ boto3/get_vpc_4.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import json
+import boto3
+
+def get_vpc_peering_details(region):
+    ec2 = boto3.client('ec2', region_name=region)
+
+    peering_connections = ec2.describe_vpc_peering_connections()
+
+    peering_details_list = []
+
+    for peering in peering_connections['VpcPeeringConnections']:
+        peering_details = {}
+        peering_details['PeeringConnectionId'] = peering['VpcPeeringConnectionId']
+        peering_details['Status'] = peering['Status']['Code']
+        peering_details['RequesterVPC'] = peering['RequesterVpcInfo']['VpcId']
+        peering_details['AccepterVPC'] = peering['AccepterVpcInfo']['VpcId']
+        
+        # Check if Tags key exists
+        if 'Tags' in peering:
+            tags = {tag['Key']: tag['Value'] for tag in peering['Tags']}
+            peering_details['Tags'] = tags
+
+        peering_details_list.append(peering_details)
+
+    return peering_details_list
+
+def get_all_vpc_peering_details():
+    ec2_client = boto3.client('ec2')
+
+    regions = ec2_client.describe_regions()['Regions']
+
+    all_peering_details = {}
+
+    for region in regions:
+        region_name = region['RegionName']
+        peering_details = get_vpc_peering_details(region_name)
+        all_peering_details[region_name] = peering_details
+
+    return all_peering_details
+
+all_peering_details = get_all_vpc_peering_details()
+print(json.dumps(all_peering_details))
